5b84326ca38497856887810b85fa2caff8995466
Denis Gamayunov <gamaunov1911@gmail.com>
2023-10-21 11:09:31 +0300

    fix users test

--- __tests__/e2e/users.api.e2e.ts
+++ __tests__/e2e/users.api.e2e.ts
@@ -2,14 +2,6 @@ import request from 'supertest'
 
 import { CreateUserModel } from '../../src/models'
 
-const EmptyOutput = {
-  pagesCount: 0,
-  page: 1,
-  pageSize: 10,
-  totalCount: 0,
-  items: [],
-}
-
 const getRequest = () => {
   return request('http://localhost:5000/')
 }
@@ -25,8 +17,106 @@ describe('users', () => {
     await getRequest().delete(`${'testing'}/all-data`)
   })
 
-  it('should return 200 and empty array', async () => {
-    await getRequest().get('users').expect(200, EmptyOutput)
+  const username = 'admin'
+  const password = 'qwerty'
+  const authHeader = encodeCredentials(username, password)
+  let createdUser: any
+
+  beforeEach(async () => {
+    //case create user with correct input data
+    const createUserData: CreateUserModel = {
+      login: 'login',
+      password: 'string123',
+      email: 'qvccgaov11@gmail.com',
+    }
+
+    const createdUserData = await getRequest()
+      .post('users')
+      .set('Authorization', authHeader)
+      .send(createUserData)
+      .expect(201)
+
+    createdUser = createdUserData.body
+
+    expect(createdUser).toEqual({
+      id: expect.any(String),
+      email: createUserData.email,
+      login: createUserData.login,
+      createdAt: expect.any(String),
+    })
+  })
+
+  it(`should find user by searchLoginTerm +
+  should find user by searchEmailTerm`, async () => {
+    const params = {
+      sortBy: 'createdAt',
+      sortDirection: 'asc',
+      pageNumber: 1,
+      pageSize: 4,
+      searchLoginTerm: 'login',
+    }
+
+    const usersWithPaging = await getRequest()
+      .get(`users`)
+      .query(params)
+      .expect(200)
+
+    const userBySearchLoginTerm = usersWithPaging.body
+    const user = userBySearchLoginTerm.items[0]
+
+    expect(userBySearchLoginTerm).toEqual({
+      pagesCount: 1,
+      page: 1,
+      pageSize: userBySearchLoginTerm.pageSize,
+      totalCount: 1,
+      items: [
+        {
+          id: user.id,
+          email: user.email,
+          login: user.login,
+          createdAt: user.createdAt,
+        },
+      ],
+    })
+
+    //should find user by searchEmailTerm
+    const paramsForEmail = {
+      sortBy: 'createdAt',
+      sortDirection: 'asc',
+      pageNumber: 1,
+      pageSize: 4,
+      searchEmailTerm: 'qv',
+    }
+
+    const usersWithPagingEmail = await getRequest()
+      .get(`users`)
+      .query(paramsForEmail)
+      .expect(200)
+
+    const userBySearchEmailTerm = usersWithPagingEmail.body
+    const user2 = userBySearchEmailTerm.items[0]
+
+    expect(userBySearchEmailTerm).toEqual({
+      pagesCount: 1,
+      page: 1,
+      pageSize: userBySearchEmailTerm.pageSize,
+      totalCount: 1,
+      items: [
+        {
+          id: user2.id,
+          email: user2.email,
+          login: user2.login,
+          createdAt: user2.createdAt,
+        },
+      ],
+    })
+  })
+
+  it(`should delete user by id`, async () => {
+    await getRequest()
+      .delete(`users/${createdUser.id}`)
+      .set('Authorization', authHeader)
+      .expect(204)
   })
 
   it(`shouldn't create user with incorrect input data`, async () => {
@@ -152,102 +242,6 @@ describe('users', () => {
       .set('Authorization', authHeader)
       .send(emailPattern)
       .expect(400)
-  })
-
-  let createdUser: any = null
-  it(`should create user with correct input data +
-  should find user by searchLoginTerm +
-  should find user by searchEmailTerm`, async () => {
-    const username = 'admin'
-    const password = 'qwerty'
-
-    const authHeader = encodeCredentials(username, password)
-
-    //case create user with correct input data
-    const createUserData: CreateUserModel = {
-      login: 'login',
-      password: 'string123',
-      email: 'qvccgaov11@gmail.com',
-    }
-
-    const createdUserData = await getRequest()
-      .post('users')
-      .set('Authorization', authHeader)
-      .send(createUserData)
-      .expect(201)
-
-    createdUser = createdUserData.body
-
-    expect(createdUser).toEqual({
-      id: expect.any(String),
-      email: createUserData.email,
-      login: createUserData.login,
-      createdAt: expect.any(String),
-    })
-
-    //case find user by searchLoginTerm
-    const params = {
-      sortBy: 'createdAt',
-      sortDirection: 'asc',
-      pageNumber: 1,
-      pageSize: 4,
-      searchLoginTerm: 'login',
-    }
-
-    const usersWithPaging = await getRequest()
-      .get(`users`)
-      .query(params)
-      .expect(200)
-
-    const userBySearchLoginTerm = usersWithPaging.body
-    const user = userBySearchLoginTerm.items[0]
-    expect(userBySearchLoginTerm).toEqual({
-      pagesCount: 1,
-      page: 1,
-      pageSize: userBySearchLoginTerm.pageSize,
-      totalCount: 1,
-      items: [
-        {
-          id: user.id,
-          email: user.email,
-          login: user.login,
-          createdAt: user.createdAt,
-        },
-      ],
-    })
-
-    //case find user by searchEmailTerm
-    const paramsForEmail = {
-      sortBy: 'createdAt',
-      sortDirection: 'asc',
-      pageNumber: 1,
-      pageSize: 4,
-      searchEmailTerm: 'aov',
-    }
-
-    const usersWithPagingEmail = await getRequest()
-      .get(`users`)
-      .query(paramsForEmail)
-      .expect(200)
-
-    const userBySearchEmailTerm = usersWithPagingEmail.body
-
-    expect(userBySearchEmailTerm).toEqual({
-      pagesCount: 1,
-      page: 1,
-      pageSize: userBySearchEmailTerm.pageSize,
-      totalCount: 1,
-      items: [
-        {
-          id: userBySearchEmailTerm.items.map((i: any) => i.id)[0],
-          email: userBySearchEmailTerm.items.map((i: any) => i.email)[0],
-          login: userBySearchEmailTerm.items.map((i: any) => i.login)[0],
-          createdAt: userBySearchEmailTerm.items.map(
-            (i: any) => i.createdAt,
-          )[0],
-        },
-      ],
-    })
   })
 
   afterAll((done) => {
